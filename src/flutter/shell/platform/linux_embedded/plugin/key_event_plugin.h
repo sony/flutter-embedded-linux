3c1bb4a59fa8f5d9fbbcbc8db778e89ff2588156
Hidenori Matsubayashi <Hidenori.Matsubayashi@sony.com>
2021-03-12 14:30:52 +0900

    Revert "unordered_map header file is insufficient"
    This reverts commit 3e9b8d6fb6704de045f16dfc63d9c0b324922c9e.

--- src/flutter/shell/platform/linux_embedded/plugin/key_event_plugin.h
+++ src/flutter/shell/platform/linux_embedded/plugin/key_event_plugin.h
@@ -9,7 +9,6 @@
 #include <xkbcommon/xkbcommon.h>
 
 #include <memory>
-#include <unordered_map>
 
 #include "flutter/shell/platform/common/client_wrapper/include/flutter/basic_message_channel.h"
 #include "flutter/shell/platform/common/client_wrapper/include/flutter/binary_messenger.h"
--- src/flutter/shell/platform/linux_embedded/window/linuxes_window_wayland.h
+++ src/flutter/shell/platform/linux_embedded/window/linuxes_window_wayland.h
@@ -9,7 +9,6 @@
 #include <wayland-cursor.h>
 
 #include <memory>
-#include <unordered_map>
 #include <vector>
 
 #include "flutter/shell/platform/linux_embedded/surface/linuxes_surface_gl_wayland.h"
