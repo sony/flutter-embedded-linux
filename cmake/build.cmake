84fabff3bb0a36edf469b60339b5b5e0e704dec4
Hidenori Matsubayashi <Hidenori.Matsubayashi@sony.com>
2021-03-17 14:56:39 +0900

    Refactor source files and add GitHub Actions for build-test of DRM backend  (#31)

--- cmake/build.cmake
+++ cmake/build.cmake
@@ -6,6 +6,7 @@ if(USE_DRM)
   add_definitions(-DDISPLAY_BACKEND_TYPE_DRM)
   set(DISPLAY_BACKEND_SRC
     src/flutter/shell/platform/linux_embedded/window/linuxes_window_drm.cc
+    src/flutter/shell/platform/linux_embedded/surface/linuxes_surface_gl_drm.cc
     src/flutter/shell/platform/linux_embedded/surface/native_window_drm.cc)
 else()
   find_program(WaylandScannerExec NAMES wayland-scanner)
@@ -30,6 +31,7 @@ else()
   set(DISPLAY_BACKEND_SRC
     ${_code}
     src/flutter/shell/platform/linux_embedded/window/linuxes_window_wayland.cc
+    src/flutter/shell/platform/linux_embedded/surface/linuxes_surface_gl_wayland.cc
     src/flutter/shell/platform/linux_embedded/surface/native_window_wayland.cc)
 endif()
 
