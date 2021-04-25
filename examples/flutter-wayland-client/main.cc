091c1978ccc6467a424e8612de18ac25f8d4e9e1
Hidenori Matsubayashi <Hidenori.Matsubayashi@sony.com>
2021-04-26 08:35:32 +0900

    Update examples

--- examples/flutter-wayland-client/main.cc
+++ examples/flutter-wayland-client/main.cc
@@ -9,32 +9,40 @@
 #include <memory>
 #include <string>
 
-static void PrintHelp() {
-  std::cout << "Usage: ./${execute filename} {Flutter project bundle path}"
-            << std::endl;
-}
+#include "command_options.h"
 
 int main(int argc, char** argv) {
-  if (argc != 2) {
-    PrintHelp();
+  commandline::CommandOptions options;
+  options.AddString("bundle", "b", "Path to Flutter app bundle", "./bundle",
+                    true);
+  options.AddWithoutValue("fullscreen", "f", "Always full-screen display",
+                          false);
+  options.AddWithoutValue("no-cursor", "n", "No mouse cursor/pointer", false);
+  options.AddInt("width", "w", "Flutter app window width", 1280, false);
+  options.AddInt("height", "h", "Flutter app window height", 720, false);
+  if (!options.Parse(argc, argv)) {
+    std::cerr << options.GetErrors() << std::endl;
+    std::cout << options.ShowHelp();
     return 0;
   }
 
-  bool show_cursor = true;
-  std::string str(argv[1]);
-  std::wstring fl_path(str.begin(), str.end());
-
   // The project to run.
+  const auto bundle_path = options.GetValue<std::string>("bundle");
+  const std::wstring fl_path(bundle_path.begin(), bundle_path.end());
   flutter::DartProject project(fl_path);
   auto command_line_arguments = std::vector<std::string>();
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   // The Flutter instance hosted by this window.
-  int width = 640;
-  int height = 480;
+  const bool show_cursor = !options.Exist("no-cursor");
+  const auto view_mode =
+      options.Exist("fullscreen")
+          ? flutter::FlutterViewController::ViewMode::kFullscreen
+          : flutter::FlutterViewController::ViewMode::kNormal;
+  const auto width = options.GetValue<int>("width");
+  const auto height = options.GetValue<int>("height");
   auto flutter_controller = std::make_unique<flutter::FlutterViewController>(
-      flutter::FlutterViewController::ViewMode::kNormal, width, height,
-      show_cursor, project);
+      view_mode, width, height, show_cursor, project);
 
   // Ensure that basic setup of the controller was successful.
   if (!flutter_controller->engine() || !flutter_controller->view()) {
