23288618f791da54078dff8010d18d23ba0c5d68
Hidenori Matsubayashi <Hidenori.Matsubayashi@sony.com>
2021-06-11 10:13:00 +0900

    Format code

--- src/flutter/shell/platform/linux_embedded/window/native_window_wayland.cc
+++ src/flutter/shell/platform/linux_embedded/window/native_window_wayland.cc
@@ -25,16 +25,19 @@ NativeWindowWayland::NativeWindowWayland(wl_compositor* compositor,
 
   // The offscreen (resource) surface will not be mapped, but needs to be a
   // wl_surface because ONLY window EGL surfaces are supported on Wayland.
-  surface_offscreen_ = wl_compositor_create_surface(compositor);
-  if (!surface_offscreen_) {
-    LINUXES_LOG(ERROR) << "Failed to create the compositor surface for off-screen.";
-    return;
-  }
+  {
+    surface_offscreen_ = wl_compositor_create_surface(compositor);
+    if (!surface_offscreen_) {
+      LINUXES_LOG(ERROR)
+          << "Failed to create the compositor surface for off-screen.";
+      return;
+    }
 
-  window_offscreen_ = wl_egl_window_create(surface_offscreen_, 1, 1);
-  if (!window_offscreen_) {
-    LINUXES_LOG(ERROR) << "Failed to create the EGL window for offscreen.";
-    return;
+    window_offscreen_ = wl_egl_window_create(surface_offscreen_, 1, 1);
+    if (!window_offscreen_) {
+      LINUXES_LOG(ERROR) << "Failed to create the EGL window for offscreen.";
+      return;
+    }
   }
 
   width_ = width;
