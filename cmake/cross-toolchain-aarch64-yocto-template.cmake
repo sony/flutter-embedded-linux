12f87b18c4d016d4e9b6571c5d5b61a0a6df4109
Hidenori Matsubayashi <Hidenori.Matsubayashi@sony.com>
2021-06-18 09:02:23 +0900

    Change comments

--- cmake/cross-toolchain-aarch64-yocto-template.cmake
+++ cmake/cross-toolchain-aarch64-yocto-template.cmake
@@ -3,9 +3,12 @@ cmake_minimum_required(VERSION 3.10)
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-# Specify the sysroot. You need to modify <path_to_user_target_sysroot> appropriately for your environment
+# Specify the sysroot.
+# You need to modify <path_to_user_target_sysroot> appropriately for your environment.
 set(target_sysroot "<path_to_user_target_sysroot>")
-# Specify the cross compiler. You need to modify <path_to_user_toolchain> appropriately for your environment
+
+# Specify the cross compiler.
+# You need to modify <path_to_user_toolchain> appropriately for your environment.
 set(toolchain "<path_to_user_toolchain>")
 
 set(CMAKE_SYSROOT ${target_sysroot})
@@ -21,4 +24,3 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
-
