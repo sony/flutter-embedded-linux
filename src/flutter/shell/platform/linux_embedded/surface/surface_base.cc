0c6bf2762e8be92f9f0cfe5a0965a4cbe3cad0bd
Hidenori Matsubayashi <Hidenori.Matsubayashi@sony.com>
2021-06-24 14:20:18 +0900

    Rename surface to surface_base (#183)
    Fixed https://github.com/sony/flutter-embedded-linux/issues/182

--- src/flutter/shell/platform/linux_embedded/surface/surface_base.cc
+++ src/flutter/shell/platform/linux_embedded/surface/surface_base.cc
@@ -2,17 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "flutter/shell/platform/linux_embedded/surface/surface.h"
+#include "flutter/shell/platform/linux_embedded/surface/surface_base.h"
 
 #include "flutter/shell/platform/linux_embedded/logger.h"
 
 namespace flutter {
 
-bool Surface::IsValid() const {
+bool SurfaceBase::IsValid() const {
   return offscreen_surface_ && context_->IsValid();
 };
 
-bool Surface::SetNativeWindow(NativeWindow* window) {
+bool SurfaceBase::SetNativeWindow(NativeWindow* window) {
   native_window_ = window;
 
   onscreen_surface_ = context_->CreateOnscreenSurface(native_window_);
@@ -29,7 +29,8 @@ bool Surface::SetNativeWindow(NativeWindow* window) {
   return true;
 };
 
-bool Surface::OnScreenSurfaceResize(const size_t width, const size_t height) {
+bool SurfaceBase::OnScreenSurfaceResize(const size_t width,
+                                        const size_t height) {
   if (!native_window_->Resize(width, height)) {
     ELINUX_LOG(ERROR) << "Failed to resize.";
     return false;
@@ -47,14 +48,16 @@ bool Surface::OnScreenSurfaceResize(const size_t width, const size_t height) {
   return true;
 };
 
-bool Surface::ClearCurrentContext() const { return context_->ClearCurrent(); };
+bool SurfaceBase::ClearCurrentContext() const {
+  return context_->ClearCurrent();
+};
 
-void Surface::DestroyOnScreenContext() {
+void SurfaceBase::DestroyOnScreenContext() {
   context_->ClearCurrent();
   onscreen_surface_ = nullptr;
 };
 
-bool Surface::ResourceContextMakeCurrent() const {
+bool SurfaceBase::ResourceContextMakeCurrent() const {
   if (!offscreen_surface_) {
     return false;
   }
