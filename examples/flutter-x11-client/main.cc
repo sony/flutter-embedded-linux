0c1914b16f089ccd9cfb8ead5c16f3b6044d0c3f
Hidenori Matsubayashi <Hidenori.Matsubayashi@sony.com>
2021-07-16 12:18:50 +0900

    Refactoring: integrate multi main.cc files (#201)
    * Refactoring: integrate multi main.cc files
    
    * Remove unused include

--- examples/flutter-x11-client/main.cc
+++ examples/flutter-x11-client/main.cc
@@ -9,46 +9,29 @@
 #include <memory>
 #include <string>
 
-#include "command_options.h"
+#include "flutter_embedder_options.h"
 #include "flutter_window.h"
 
 int main(int argc, char** argv) {
-  commandline::CommandOptions options;
-  options.AddString("bundle", "b", "Path to Flutter app bundle", "./bundle",
-                    true);
-  options.AddWithoutValue("fullscreen", "f", "Always full-screen display",
-                          false);
-  options.AddWithoutValue("no-cursor", "n", "No mouse cursor/pointer", false);
-  options.AddInt("width", "w", "Flutter app window width", 1280, false);
-  options.AddInt("height", "h", "Flutter app window height", 720, false);
+  FlutterEmbedderOptions options;
   if (!options.Parse(argc, argv)) {
-    std::cerr << options.GetError() << std::endl;
-    std::cout << options.ShowHelp();
     return 0;
   }
 
-  // The project to run.
-  const bool show_cursor = !options.Exist("no-cursor");
-  const auto view_mode =
-      options.Exist("fullscreen")
-          ? flutter::FlutterViewController::ViewMode::kFullscreen
-          : flutter::FlutterViewController::ViewMode::kNormal;
-  const auto width = options.GetValue<int>("width");
-  const auto height = options.GetValue<int>("height");
-  const auto bundle_path = options.GetValue<std::string>("bundle");
-
+  // Creates the Flutter project.
+  const auto bundle_path = options.BundlePath();
   const std::wstring fl_path(bundle_path.begin(), bundle_path.end());
   flutter::DartProject project(fl_path);
   auto command_line_arguments = std::vector<std::string>();
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   flutter::FlutterViewController::ViewProperties view_properties = {};
-  view_properties.width = width;
-  view_properties.height = height;
-  view_properties.view_mode = view_mode;
-  view_properties.use_mouse_cursor = show_cursor;
-  view_properties.use_onscreen_keyboard = false;
-  view_properties.use_window_decoration = false;
+  view_properties.width = options.WindowWidth();
+  view_properties.height = options.WindowHeight();
+  view_properties.view_mode = options.WindowViewMode();
+  view_properties.use_mouse_cursor = options.IsUseMouseCursor();
+  view_properties.use_onscreen_keyboard = options.IsUseOnscreenKeyboard();
+  view_properties.use_window_decoration = options.IsUseWindowDecoraation();
 
   // The Flutter instance hosted by this window.
   FlutterWindow window(view_properties, project);
@@ -58,5 +41,6 @@ int main(int argc, char** argv) {
   }
   window.Run();
   window.OnDestroy();
+
   return 0;
 }
