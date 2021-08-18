b6187cdc60a74a31808cfd8c6e5be4986a65cf15
Hidenori Matsubayashi <Hidenori.Matsubayashi@sony.com>
2021-08-18 13:41:51 +0900

    [Wayland] Delete weston desktop shell (#212)
    See: https://github.com/sony/flutter-embedded-linux/issues/189

--- examples/flutter-drm-gbm-backend/cmake/user_config.cmake
+++ examples/flutter-drm-gbm-backend/cmake/user_config.cmake
@@ -3,5 +3,4 @@ cmake_minimum_required(VERSION 3.10)
 # Flutter embedder configurations.
 # See: https://github.com/sony/flutter-embedded-linux/wiki/Building-Embedded-Linux-embedding-for-Flutter#user-configuration-parameters-cmake-options
 set(BACKEND_TYPE DRM-GBM)
-set(DESKTOP_SHELL OFF)
 set(USE_GLES3 OFF)
